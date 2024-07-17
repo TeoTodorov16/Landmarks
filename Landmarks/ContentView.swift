8c7e6b530ec4336255292b93ee1ece84753735a5
Teodor <teodorttodorov@icloud.com>
2024-07-17 15:02:31 +0300

    Adding another VStack to render the other components.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack{
-                Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+           
+            
+            
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack{
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            
+           
         }
-        .padding()
     }
 }
 
